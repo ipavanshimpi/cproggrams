91afe0bd0214764fd4436aa50430a72b91b7e0dd
Pavan Milind Shimpi <143185354+ipavanshimpi@users.noreply.github.com>
2023-09-07 23:51:20 +0530

    Add files via upload

---
+++ oddeven.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("Enter your number :-");
+scanf("%d",&a);
+if(a/2 == 0)
+{
+printf("Number is even");
+}
+else
+{
+printf("your number is odd");
+}
+}
