cb0a07c60fc6c13768cecbabcb921bd104c1f0c1
Pavan Milind Shimpi <143185354+ipavanshimpi@users.noreply.github.com>
2023-09-07 23:33:05 +0530

    Add files via upload

---
+++ forceformula.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int M1;
+int M2;
+printf("Enter the mass of first object:- ");
+scanf("%d",&M1);
+printf("Enter the mass of second object:- ");
+scanf("%d",&M2);
+float G;
+G == 6.287;
+int rad;
+printf("Enter the distance between objects:- ");
+scanf("%d",&rad);
+int force;
+force == G*M1*M2/rad;
+printf("Gravitation force between them:- ",&force);
+}
